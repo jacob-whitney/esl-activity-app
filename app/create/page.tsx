27a1b0e36d482bbe99c3ac002b4f0f013af0cbb1
jacobw <jacobmwhitney1@gmail.com>
2026-09-20 20:35:47 +0000

    Re-add create page/form

---
+++ app/create/page.tsx
@@ -0,0 +1,9 @@
+import Create from '@/app/ui/forms/create';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <Create />
+    </main>
+  );
+}
