f7046be117e7c697a4b948fcc2de5ab3690f30a0
jacobw <jacobmwhitney1@gmail.com>
2026-09-22 17:06:52 +0000

    Setup .env and allow browser connection via public IP address

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [`http://${process.env.HOST_IP}:3000`]
 };
+
+/* See https://nextjs.org/docs/pages/guides/environment-variables */
 
 export default nextConfig;
